28d7b94314cd6403c5f525358ecdfda5f7dcc03a
tiago-falves <tiago.falves98@gmail.com>
2019-06-29 22:33:01 +0200

    Exame

--- Exame/Modules/estudo.py
+++
@@ -1,8 +0,0 @@
-def recursion_depth(number):
-    print("Recursion depth number", number)
-    try:
-        recursion_depth(number + 1)
-    except:
-        print("I cannot go any deeper into this wormhole.")
-        recursion_depth(0)
-recursion_depth(5)
